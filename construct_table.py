ad7f7e789846a70d3fb05b8cae0e3bb5c30c77cb
kangwoosukeq <kangwoosuk1@gmail.com>
2019-11-22 05:34:25 +0900

    construct 64bit table

--- construct_table.py
+++ construct_table.py
@@ -2,14 +2,14 @@ def d_term(x, s):
     numer = (2 ** 225)
     denom = 1
     result = numer // denom
-    pi70 = 31415926535897932384626433832795028841971693993751058209749445923078164
-    for i in range(500):
-        numer = numer * pi70
+    pi20 = 314159265358979323846#26433832795028841971693993751058209749445923078164
+    for i in range(300):
+        numer = numer * pi20
         numer = numer * (x * x)
 
         denom = denom * (i + 1)
         denom = denom * (s * s)
-        denom = denom * (10 ** 70)
+        denom = denom * (10 ** 20)
         taylor = numer // denom
 
         if i % 2 == 0:
@@ -35,5 +35,5 @@ if __name__ == "__main__":
         table[i] = table[i] + ((2 ** 224) // s)
 
     for value in table:
-        f.write(hex(value >> 32))
+        f.write(hex(value >> (32 + 128)))
         f.write('\n')
